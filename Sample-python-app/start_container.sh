669ee3d77df835e2720981e56c7b8ec396dadba8
Lakshmi Narayanan <97275577+r-narayanan4@users.noreply.github.com>
2023-12-18 12:52:30 +0530

    Update start_container.sh

--- Sample-python-app/start_container.sh
+++ Sample-python-app/start_container.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+set -e
 
+# Pull the Docker image from Docker Hub
+docker pull rln410/simple-python-flask-app:latest
+
+# Run the Docker image as a container
+docker run -d -p 4000:4000 rln410/simple-python-flask-app:latest
